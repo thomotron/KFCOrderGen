0dbcefc9bb2257d52f4efb46ed29447c03e3f895
Tom <gigatech55@gmail.com>
2016-02-11 21:08:28 +1030

    Added support for sizes and combos

--- menu.js
+++ menu.js
@@ -5,346 +5,618 @@ var menu = {
     },
     {
       "name": "Original Recipe Burger",
-	  "price": 5.95
+      "price": 5.95,
+      "combo": {
+        "reg": {
+          "name": "Original Recipe Burger Combo",
+          "price": 7.95
+        },
+        "lrg": {
+          "name": "Original Recipe Burger Combo Large",
+          "price": 9.95
+        }
+      }
     },
     {
       "name": "Original BBQ Bacon & Cheese Burger",
-	  "price": 6.95
+      "price": 6.95,
+      "combo": {
+        "reg": {
+          "name": "Original BBQ Bacon & Cheese Burger Combo",
+          "price": 8.95
+        },
+        "lrg": {
+          "name": "Original BBQ Bacon & Cheese Burger Combo Large",
+          "price": 10.95
+        }
+      }
     },
     {
       "name": "Original BLT Burger",
-	  "price": 6.95
+      "price": 6.95,
+      "combo": {
+        "reg": {
+          "name": "Original BLT Burger Combo",
+          "price": 8.95
+        },
+        "lrg": {
+          "name": "Original BLT Burger Combo Large",
+          "price": 10.95
+        }
+      }
     },
     {
       "name": "Original Recipe Bacon & Cheese Burger",
-	  "price": 6.95
+      "price": 6.95,
+      "combo": {
+        "reg": {
+          "name": "Original Recipe Bacon & Cheese Burger Combo",
+          "price": 8.95
+        },
+        "lrg": {
+          "name": "Original Recipe Bacon & Cheese Burger Combo Large",
+          "price": 10.95
+        }
+      }
     },
     {
       "name": "Zinger Burger",
-	  "price": 5.95
+      "price": 5.95,
+      "combo": {
+        "reg": {
+          "name": "Zinger Burger Combo",
+          "price": 7.95
+        },
+        "lrg": {
+          "name": "Zinger Burger Combo Large",
+          "price": 9.95
+        }
+      }
     },
     {
       "name": "Kentucky Burger",
-	  "price": 7.95
+      "price": 7.95,
+      "combo": {
+        "reg": {
+          "name": "Kentucky Burger Combo",
+          "price": 10.5
+        },
+        "lrg": {
+          "name": "Kentucky Burger Combo Large",
+          "price": 11.95
+        }
+      }
     },
     {
       "name": "Zinger Stacker Burger",
-	  "price": 8.95
+      "price": 8.95,
+      "combo": {
+        "reg": {
+          "name": "Zinger Stacker Burger Combo",
+          "price": 10.95
+        },
+        "lrg": {
+          "name": "Zinger Stacker Burger Combo Large",
+          "price": 10.45
+        }
+      }
     },
     {
       "name": "Zinger Bacon and Cheese Burger",
-	  "price": 6.95
+      "price": 6.95,
+      "combo": {
+        "reg": {
+          "name": "Zinger Bacon and Cheese Burger Combo",
+          "price": 8.95
+        },
+        "lrg": {
+          "name": "Zinger Bacon and Cheese Burger Combo Large",
+          "price": 10.95
+        }
+      }
     },
     {
       "name": "Double Crunch Burger",
-	  "price": 4.95
+      "price": 4.95,
+      "combo": {
+        "reg": {
+          "name": "Double Crunch Burger Combo",
+          "price": 6.95
+        },
+        "lrg": {
+          "name": "Double Crunch Burger Combo Large",
+          "price": 8.95
+        }
+      }
     },
     {
       "name": "----------------------- Chicken ----------------------"
     },
     {
       "name": "Original Recipe Chicken 1 Piece",
-	  "price": 3.00
+      "price": 3
     },
     {
       "name": "Original Recipe Chicken 5 Pieces",
-	  "price": 12.95
+      "price": 12.95
     },
     {
       "name": "Original Tenders 5 pieces",
-	  "price": 9.95
+      "price": 9.95,
+      "combo": {
+        "reg": {
+          "name": " Combo",
+          "price": 6.95
+        },
+        "lrg": {
+          "name": " Combo Large",
+          "price": 8.95
+        }
+      }
     },
     {
       "name": "Original Tenders 3 pieces",
-	  "price": 6.95
+      "price": 6.95,
+      "combo": {
+        "reg": {
+          "name": " Combo",
+          "price": 6.95
+        },
+        "lrg": {
+          "name": " Combo Large",
+          "price": 8.95
+        }
+      }
     },
-    /*{ Only in WA and QLD
-      "name": "Hot & Spicy Chicken 1 piece",
-	  "price": 9001.00
-    },*/
     {
       "name": "Original Recipe Chicken 21 Pieces",
-	  "price": 34.95
+      "price": 34.95
     },
     {
       "name": "Original Tenders SoSalad",
-	  "price": 7.95
+      "price": 7.95
     },
     {
       "name": "Wicked Wings 3 pieces",
-	  "price": 3.50
+      "price": 3.5
     },
     {
       "name": "Wicked Wings 6 pieces",
-	  "price": 5.95
+      "price": 5.95
     },
     {
       "name": "Wicked Wings 10 pieces",
-	  "price": 9.95
+      "price": 9.95
     },
     {
       "name": "Kentucky Nuggets 6 Pieces",
-	  "price": 5.95
+      "price": 5.95
     },
     {
       "name": "Kentucky Nuggets 10 Pieces",
-	  "price": 8.95
+      "price": 8.95
     },
     {
       "name": "Popcorn Chicken Regular",
-	  "price": 5.95
+      "price": 5.95,
+      "combo": {
+        "reg": {
+          "name": "Popcorn Chicken Regular Combo",
+          "price": 8.95
+        }
+      }
     },
     {
       "name": "Popcorn Chicken Maxi",
-	  "price": 7.95
+      "price": 7.95,
+      "combo": {
+        "reg": {
+          "name": "Popcorn Chicken Maxi Combo",
+          "price": 10.95
+        }
+      }
     },
     {
       "name": "Grilled Strip Salad",
-	  "price": 7.95
+      "price": 7.95
     },
     {
       "name": "-------------------- Boxed Meals ---------------------"
     },
     {
       "name": "Original Tenders Box",
-	  "price": 10.95
+      "price": 10.95
     },
     {
       "name": "Kentucky Burger Boxed Meal",
-	  "price": 11.95
+      "price": 11.95
     },
     {
       "name": "Zinger Stacker Boxed meal",
-	  "price": 13.45
+      "price": 13.45
     },
     {
       "name": "Ultimate Box",
-	  "price": 9.95
+      "price": 9.95
     },
     {
       "name": "Zinger Box",
-	  "price": 10.45
+      "price": 10.45
     },
     {
       "name": "Twister Box",
-	  "price": 11.25
+      "price": 11.25
     },
     {
       "name": "3 Piece Box",
-	  "price": 10.95
+      "price": 10.95
     },
     {
       "name": "5 Stars Box",
-	  "price": 12.25
+      "price": 12.25
     },
     {
       "name": "---------------------- Twisters ----------------------"
     },
     {
       "name": "Regular Twister",
-	  "price": 6.35
+      "price": 6.35,
+      "combo": {
+        "reg": {
+          "name": "Regular Twister Combo",
+          "price": 8.95
+        },
+        "lrg": {
+          "name": "Regular Twister Combo Large",
+          "price": 10.95
+        }
+      }
     },
     {
       "name": "Mex Fresh Twister",
-	  "price": 6.95
+      "price": 6.95,
+      "combo": {
+        "reg": {
+          "name": "Mex Fresh Twister Combo",
+          "price": 9.95
+        },
+        "lrg": {
+          "name": "Mex Fresh Twister Combo Large",
+          "price": 11.95
+        }
+      }
     },
     {
       "name": "BLT Twister",
-	  "price": 6.95
+      "price": 6.95,
+      "combo": {
+        "reg": {
+          "name": "BLT Twister Combo",
+          "price": 9.95
+        },
+        "lrg": {
+          "name": "BLT Twister Combo Large",
+          "price": 11.95
+        }
+      }
     },
     {
       "name": "Spicy Hot Twister",
-	  "price": 6.95
+      "price": 6.95,
+      "combo": {
+        "reg": {
+          "name": "Spicy Hot Twister Combo",
+          "price": 9.95
+        },
+        "lrg": {
+          "name": "Spicy Hot Twister Combo Large",
+          "price": 11.95
+        }
+      }
     },
     {
       "name": "Sweet Chilli Twister",
-	  "price": 6.35
+      "price": 6.35,
+      "combo": {
+        "reg": {
+          "name": "Sweet Chilli Twister Combo",
+          "price": 8.95
+        },
+        "lrg": {
+          "name": "Sweet Chilli Twister Combo Large",
+          "price": 10.95
+        }
+      }
     },
     {
       "name": "Chicken Salad Twister",
-	  "price": 6.95
+      "price": 6.95,
+      "combo": {
+        "reg": {
+          "name": "Chicken Salad Twister Combo",
+          "price": 9.95
+        },
+        "lrg": {
+          "name": "Chicken Salad Twister Combo Large",
+          "price": 11.95
+        }
+      }
     },
     {
       "name": "-------------------- Group Meals ---------------------"
     },
     {
       "name": "Original Tenders Variety",
-	  "price": 32.95
+      "price": 32.95
     },
     {
       "name": "Family Feast",
-	  "price": 28.95
+      "price": 28.95
     },
     {
       "name": "Streetwise Feast",
-	  "price": 19.95
+      "price": 19.95
     },
     {
       "name": "Family Burger Box",
-	  "price": 25.95
+      "price": 25.95
     },
     {
       "name": "Giant Feast",
-	  "price": 41.95
+      "price": 41.95
     },
     {
       "name": "----------------------- Snacks -----------------------"
     },
     {
       "name": "Popcorn Chicken Go Bucket",
-	  "price": 3.75
+      "price": 3.75
     },
     {
       "name": "Nuggets Go Bucket",
-	  "price": 3.75
+      "price": 3.75
     },
     {
       "name": "Wicked Wing Go Bucket",
-	  "price": 3.75
+      "price": 3.75
     },
     {
       "name": "Original Tenders Go Bucket",
-	  "price": 3.75
+      "price": 3.75
     },
     {
       "name": "Grilled Taster Box",
-	  "price": 3.75
+      "price": 3.75
     },
     {
       "name": "BLT Snack Twister",
-	  "price": 2.95
+      "price": 2.95
     },
     {
       "name": "Spicy Hot Snack Twister",
-	  "price": 2.95
+      "price": 2.95
     },
     {
       "name": "-------------------- Not hungry? ---------------------"
     },
     {
       "name": "Kids Mini Wrap Meal",
-	  "price": 4.95
+      "price": 4.95
     },
     {
       "name": "Kids Popcorn Meal",
-	  "price": 4.95
+      "price": 4.95
     },
     {
       "name": "---------------------- Krushers ----------------------"
     },
     {
       "name": "Caramel Popcorn Krusher",
-	  "price": 2.00
+      "price": 2
     },
     {
       "name": "Golden Gaytime Krusher",
-	  "price": 3.95
+      "price": 3.95
     },
     {
       "name": "Iced Mocha Krusher",
-	  "price": 3.95
+      "price": 3.95
     },
     {
       "name": "Kookies &#39;n Kream Krusher",
-	  "price": 3.95
+      "price": 3.95
     },
     {
       "name": "Mint Choc Krusher",
-	  "price": 3.95
+      "price": 3.95
     },
     {
       "name": "----------------------- Drinks -----------------------"
     },
     {
-      "name": "Pepsi 375ml",
-	  "price": 2.95
+      "name": "Pepsi",
+      "sml": {
+        "name": " 375ml",
+        "price": 2.95
+      },
+      "reg": {
+        "name": " 600ml",
+        "price": 3.95
+      },
+      "lrg": {
+        "name": " 1.25L",
+        "price": 4.25
+      }
     },
     {
-      "name": "Pepsi Max 375ml",
-	  "price": 2.95
+      "name": "Pepsi Max",
+      "sml": {
+        "name": "Pepsi Max 375ml",
+        "price": 2.95
+      },
+      "reg": {
+        "name": "Pepsi Max 600ml",
+        "price": 3.95
+      },
+      "lrg": {
+        "name": "Pepsi Max 1.25L",
+        "price": 4.25
+      }
     },
     {
-      "name": "Solo 375ml",
-	  "price": 2.95
+      "name": "Solo",
+      "sml": {
+        "name": "Solo 375ml",
+        "price": 2.95
+      },
+      "reg": {
+        "name": "Solo 600ml",
+        "price": 3.95
+      },
+      "lrg": {
+        "name": "Solo 1.25L",
+        "price": 4.25
+      }
     },
     {
-      "name": "Sunkist 375ml",
-	  "price": 2.95
+      "name": "Sunkist",
+      "sml": {
+        "name": "Sunkist 375ml",
+        "price": 2.95
+      },
+      "reg": {
+        "name": "Sunkist 600ml",
+        "price": 3.95
+      },
+      "lrg": {
+        "name": "Sunkist 1.25L",
+        "price": 4.25
+      }
     },
     {
-      "name": "7Up 375 ml",
-	  "price": 2.95
+      "name": "7Up",
+      "sml": {
+        "name": "7Up 375ml",
+        "price": 2.95
+      },
+      "reg": {
+        "name": "7Up 600ml",
+        "price": 3.95
+      },
+      "lrg": {
+        "name": "7Up 1.25L",
+        "price": 4.25
+      }
     },
     {
-      "name": "Mountain Dew 375ml",
-	  "price": 2.95
+      "name": "Mountain Dew",
+      "sml": {
+        "name": "Mountain Dew 375ml",
+        "price": 2.95
+      },
+      "reg": {
+        "name": "Mountain Dew 600ml",
+        "price": 3.95
+      },
+      "lrg": {
+        "name": "Mountain Dew 1.25L",
+        "price": 4.25
+      }
     },
     {
       "name": "Cool Ridge Water",
-	  "price": 2.95
+      "price": 2.95
     },
     {
       "name": "Spring Valley Apple Juice 250ml",
-	  "price": 2.95
+      "price": 2.95
     },
     {
       "name": "Spring Valley Orange Juice 250ml",
-	  "price": 2.95
+      "price": 2.95
     },
     {
       "name": "-------------------- Extras --------------------"
     },
     {
-      "name": "Seasoned Chips Regular",
-	  "price": 2.95
+      "name": "Seasoned Chips",
+      "reg": {
+        "name": "Seasoned Chips Regular",
+        "price": 2.95
+      },
+      "lrg": {
+        "name": "Seasoned Chips Large",
+        "price": 4.75
+      }
     },
     {
-      "name": "Potato & Gravy Regular",
-	  "price": 3.75
+      "name": "Potato & Gravy",
+      "reg": {
+        "name": "Potato & Gravy Regular",
+        "price": 3.75
+      },
+      "lrg": {
+        "name": "Potato & Gravy Large",
+        "price": 4.95
+      }
     },
     {
       "name": "So Salad",
-	  "price": 4.95
+      "price": 4.95
     },
     {
-      "name": "Coleslaw Regular",
-	  "price": 3.75
+      "name": "Coleslaw",
+      "reg": {
+        "name": "Coleslaw Regular",
+        "price": 3.75
+      },
+      "lrg": {
+        "name": "Coleslaw Large",
+        "price": 5.95
+      }
     },
     {
       "name": "Chocolate Mousse",
-	  "price": 2.50
+      "price": 2.5
     },
     {
       "name": "Dinner Roll",
-	  "price": 0.95
+      "price": 0.95
     },
     {
-      "name": "Gravy Regular",
-	  "price": 3.75
+      "name": "Gravy",
+      "reg": {
+        "name": "Gravy Regular",
+        "price": 3.75
+      },
+      "lrg": {
+        "name": "Gravy Large",
+        "price": 3.75
+      }
     },
     {
       "name": "Sauce - Tomato",
-	  "price": 0.50
+      "price": 0.5
     },
     {
       "name": "Sauce - BBQ",
-	  "price": 0.50
+      "price": 0.5
     },
     {
       "name": "Sauce - Sweet & Sour",
-	  "price": 0.50
+      "price": 0.5
     },
     {
       "name": "Sauce - Super Charged",
-	  "price": 0.50
+      "price": 0.5
     },
     {
       "name": "Sauce - Sweet Chilli",
-	  "price": 0.50
+      "price": 0.5
     },
     {
       "name": "Sauce - Creamy Aioli",
-	  "price": 0.50
+      "price": 0.5
     }
   ]
 };
